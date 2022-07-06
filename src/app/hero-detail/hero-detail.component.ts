cd199e6c81a933aa41d8691ecf717f74551daa5f
Sudhakar Verma <sverma@athenasoft.co.com>
2022-07-06 18:59:10 +0530

    heroes project

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -4,10 +4,11 @@ import { Hero } from '../hero';
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.sass']
+  styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
   @Input() hero?: Hero;
+  
   constructor() { }
 
   ngOnInit(): void {
--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -3,7 +3,7 @@ import { MessageService } from '../message.service';
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
-  styleUrls: ['./messages.component.sass']
+  styleUrls: ['./messages.component.css']
 })
 export class MessagesComponent implements OnInit {
 
